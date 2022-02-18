9b84ef54950a45c829ef73f3e64038f7cb6253ac
Himanshuary <himanshuarya2410@gmail.com>
2022-02-18 08:32:31 +0530

    intial commit

--- Array/Intial_array.py
+++
@@ -1,5 +0,0 @@
-# Creating a array in pyhon
-
-from array import *
-arr = ("i",[10,20,30,40,50])
-print(arr)
---
+++ Intial_array.py
@@ -0,0 +1,5 @@
+# Creating a array using python
+from array import*
+
+arr = ('i', [10,20,30,40,50])
+print(arr)
