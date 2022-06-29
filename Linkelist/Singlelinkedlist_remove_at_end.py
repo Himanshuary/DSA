0321dc70ea77c0a2b8f0751b3437a35d445dacc4
Himanshuary <himanshuarya2410@gmail.com>
2022-06-29 12:12:22 +0530

    intial commit

--- Linkelist/Singlelinkedlist_remove_at_end.py
+++ Linkelist/Singlelinkedlist_remove_at_end.py
@@ -20,6 +20,15 @@ class Linkedlist:
         new_node = Node(data)
         new_node.ref = self.head
         self.head = new_node
+    
+    def remove_end(self):
+        if self.head is None:
+            print("Linkedlist is empty")
+        else:
+            n = self.head
+            while n.ref.ref is not None:
+                n = n.ref
+            n.ref = None
 
 ll1 = Linkedlist()
 ll1.add_begin(10)
@@ -27,4 +36,5 @@ ll1.add_begin(20)
 ll1.add_begin(30)
 ll1.add_begin(40)
 ll1.add_begin(50)
+ll1.remove_end()
 ll1.print__LL()
--- Linkelist/Singlelinkedlist_remove_element_at_begin.py
+++ Linkelist/Singlelinkedlist_remove_element_at_begin.py
@@ -21,10 +21,18 @@ class Linkedlist:
         new_node.ref = self.head
         self.head = new_node
 
+
+    def remove_beginning(self):
+        if self.head is None:
+            print("Linkedlist is empty")
+        else:
+            self.head = self.head.ref
+
 ll1 = Linkedlist()
 ll1.add_begin(10)
 ll1.add_begin(20)
 ll1.add_begin(30)
 ll1.add_begin(40)
 ll1.add_begin(50)
+ll1.remove_beginning()
 ll1.print__LL()
