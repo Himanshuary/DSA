016dc50aa434d42c3a110067bd44f61ce0ccf18f
Himanshuary <himanshuarya2410@gmail.com>
2022-06-29 12:47:57 +0530

    intial commit

--- Linkelist/Singlelinkedlist_delete_any_node.py
+++ Linkelist/Singlelinkedlist_delete_any_node.py
@@ -21,16 +21,22 @@ class Linkedlist:
         new_node.ref = self.head
         self.head = new_node
     
-    def remove_end(self):
+    def delete_any_node(self,x):
         if self.head is None:
-            print("Linkedlist is empty")
-        elif self.head.ref is None:
-            self.head = None
+            print("LL is empty")
+            return
+        if x == self.head.data:
+            self.head = self.head.ref
+            return
+        n = self.head
+        while n.ref is not None:
+            if n.ref.data == x:
+                break
+            n = n.ref
+        if n.ref is None:
+            print("Node is not present in LL")
         else:
-            n = self.head
-            while n.ref.ref is not None:
-                n = n.ref
-            n.ref = None
+            n.ref = n.ref.ref
 
 ll1 = Linkedlist()
 ll1.add_begin(10)
@@ -38,5 +44,7 @@ ll1.add_begin(20)
 ll1.add_begin(30)
 ll1.add_begin(40)
 ll1.add_begin(50)
-ll1.remove_end()
+ll1.delete_any_node(30)
+ll1.delete_any_node(100)
+ll1.delete_any_node(50)
 ll1.print__LL()
