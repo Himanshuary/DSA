5b3da8089cb4c76786f373f26232ce5294a1bd2d
Himanshuary <himanshuarya2410@gmail.com>
2022-07-09 07:49:41 +0530

    intial commit

---
+++ Sorting/Bubble_sort.py
@@ -0,0 +1,11 @@
+def Bubble_sort(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(i+1,n):
+            if array[j] < array[i]:
+                array[j], array[i] = array[i], array[j]
+    
+    return array
+
+arr = [-2,45,0,11,-9]
+print(Bubble_sort(arr))
